959bc57e4bdf977542bdc2202171337c9b29a4c0
Lyon Singleton <lyonws@hawaii.edu>
2024-04-19 13:26:56 -1000

    added functions and structs

--- elf_parser.c
+++ elf_parser.c
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////
 ///         University of Hawaii, College of Engineering
-/// @brief  sre_lab6_readelf-lyons811 - EE 205 - Spr 2022
+/// @brief  sre_lab6_readelf-lyons811 - EE 491F - Spr 2024
 ///
 /// @file elf_parser.c
 /// @version 1.0
@@ -9,3 +9,81 @@
 /// @date   19_Apr_2024
 ///////////////////////////////////////////////////////////////////////////////
 #include "elf_parser.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Read the ELF header from the file
+void elf_read_header(const char* filename, ElfHeader64* header) {
+    FILE* file = fopen(filename, "rb");
+    if (file == NULL) {
+        perror("Error opening file");
+        exit(1);
+    }
+    if (fread(header, sizeof(ElfHeader64), 1, file) != 1) {
+        perror("Error reading file");
+        exit(1);
+    }
+    fclose(file);
+}
+// Read the section header from the file
+void elf_read_section_header(const char* filename, uint64_t offset, ElfSectionHeader64* header) {
+    FILE *file = fopen(filename, "rb");
+    if (file == NULL) {
+        perror("Error opening file");
+        exit(1);
+    }
+    if (fseek(file, offset, SEEK_SET) != 0) {
+        perror("Error seeking file");
+        exit(1);
+    }
+    if (fread(header, sizeof(ElfSectionHeader64), 1, file) != 1) {
+        perror("Error reading file");
+        exit(1);
+    }
+    fclose(file);
+}
+
+// Print the ELF header
+void elf_print_header(const ElfHeader64* header) {
+    printf("ELF Header:\n");
+    printf("  Type: %d\n", header->e_type);
+    printf("  Machine: %d\n", header->e_machine);
+    printf("  Version: %d\n", header->e_version);
+    printf("  Entry point address: %llu\n", header->e_entry);
+    printf("  Program header offset: %llu\n", header->e_phoff);
+    printf("  Section header offset: %llu\n", header->e_shoff);
+    printf("  Flags: %d\n", header->e_flags);
+    printf("  ELF header size: %d\n", header->e_ehsize);
+    printf("  Program header entry size: %d\n", header->e_phentsize);
+    printf("  Number of program header entries: %d\n", header->e_phnum);
+    printf("  Section header entry size: %d\n", header->e_shentsize);
+    printf("  Number of section header entries: %d\n", header->e_shnum);
+    printf("  Section string table index: %d\n", header->e_shstrndx);
+}
+
+// Print the section header
+void elf_print_section_header(const ElfSectionHeader64* header) {
+    printf("Section Header:\n");
+    printf("  Name: %d\n", header->sec_name);
+    printf("  Type: %d\n", header->sec_type);
+    printf("  Flags: %llu\n", header->sec_flags);
+    printf("  Address: %llu\n", header->sec_addr);
+    printf("  Offset: %llu\n", header->sec_offset);
+    printf("  Size: %llu\n", header->sec_size);
+    printf("  Link: %d\n", header->sec_link);
+    printf("  Info: %d\n", header->sec_info);
+    printf("  Address alignment: %llu\n", header->sec_addralign);
+    printf("  Entry size: %llu\n", header->sec_entsize);
+}
+
+// Print the symbol
+void elf_print_symbol(const ElfSymbol64* symbol) {
+    printf("Symbol:\n");
+    printf("  Name: %d\n", symbol->sym_name);
+    printf("  Info: %d\n", symbol->sym_info);
+    printf("  Other: %d\n", symbol->sym_other);
+    printf("  Section index: %d\n", symbol->sym_shndx);
+    printf("  Value: %llu\n", symbol->sym_value);
+    printf("  Size: %llu\n", symbol->sym_size);
+}
--- elf_parser.h
+++ elf_parser.h
@@ -10,5 +10,54 @@
 ///////////////////////////////////////////////////////////////////////////////
 #ifndef SRE_LAB6_READELF_LYONS811_ELF_PARSER_H
 #define SRE_LAB6_READELF_LYONS811_ELF_PARSER_H
+#include <stdint.h>
+// ELF Header
+typedef struct {
+    unsigned char e_ident[16]; // ELF identification
+    uint16_t e_type; // Object file type
+    uint16_t e_machine; // Machine type
+    uint32_t e_version; // Object file version
+    uint64_t e_entry; // Entry point address
+    uint64_t e_phoff; // Program header offset
+    uint64_t e_shoff; // Section header offset
+    uint32_t e_flags; // Processor-specific flags
+    uint16_t e_ehsize; // ELF header size
+    uint16_t e_phentsize; // Size of program header entry
+    uint16_t e_phnum; // Number of program header entries
+    uint16_t e_shentsize; // Size of section header entry
+    uint16_t e_shnum; // Number of section header entries
+    uint16_t e_shstrndx; // Section name string table index
+} ElfHeader64;
+
+// Section Header
+typedef struct {
+    uint32_t sec_name; // section name
+    uint32_t sec_type; // section type
+    uint64_t sec_flags; // section flags
+    uint64_t sec_addr; // section virtual address
+    uint64_t sec_offset; // section file offset
+    uint64_t sec_size; // section size in bytes
+    uint32_t sec_link; // link to another section
+    uint32_t sec_info; //  section information
+    uint64_t sec_addralign; // section alignment
+    uint64_t sec_entsize; // entry size
+} ElfSectionHeader64;
+
+// Symbol Table
+typedef struct {
+    uint32_t sym_name; // symbol name
+    uint8_t sym_info; // symbol info
+    uint8_t sym_other; // symbol other
+    uint16_t sym_shndx; // symbol section index
+    uint64_t sym_value; // symbol value
+    uint64_t sym_size; // symbol size
+} ElfSymbol64;
+
+// Functions
+void elf_read_header(const char* filename, ElfHeader64* header);
+void elf_read_section_header(const char* filename, uint64_t offset, ElfSectionHeader64* header);
+void elf_print_header(const ElfHeader64* header);
+void elf_print_section_header(const ElfSectionHeader64* header);
+void elf_print_symbol(const ElfSymbol64* symbol);
 
 #endif //SRE_LAB6_READELF_LYONS811_ELF_PARSER_H
